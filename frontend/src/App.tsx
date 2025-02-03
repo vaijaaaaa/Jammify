57df0ffc6fbd39e29c53dc8d641864655c0630bd
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-03 20:24:40 +0530

    shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-
+import {Button} from "./components/ui/button";
 function App() {
  
   
 
   return (
+    <>
+    
     <h1 className="bg-orange-500">Hello</h1>
+    <Button variant={"ghost"}>Press Me</Button>
+
+
+    </>
   )
 }
 

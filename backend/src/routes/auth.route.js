b0dfc77da77e43caa87c23a43f058bf48d3023e0
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-05 22:33:02 +0530

    auth callback

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,22 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/callback",async (req,res)=>{
-  
+  try {
+    const {id, firstName, lastName, imageUrl} = req.body;
+
+    const user = await UserActivation.findOne({clerkId: id});
+
+    if(!user){
+        await UserActivation.create({
+            clerkId:id,
+            fullName: `${firstName} ${lastName}`,
+            imageUrl
+        })
+    }
+    res.send(200).json({success:true})
+  } catch (error) {
+    res.status(500).json({message:"Internal server error",error})
+  }
 
 })
 

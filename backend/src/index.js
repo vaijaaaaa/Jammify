3113f5997fe39485636cab12ec1e773775f3f3f1
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-06 21:13:15 +0530

    middilware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import {clerkMiddleware} from "@clerk/express";
 
 
 import { connectDB } from "./lib/db.js";
@@ -21,6 +22,8 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 
 app.use("/api/users",userRoutes)

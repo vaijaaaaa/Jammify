be218e76476b31de5eb99242d17fed0ffd9dd7cb
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-06 21:50:13 +0530

    file upload setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import {clerkMiddleware} from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 
 import { connectDB } from "./lib/db.js";
-
-
 import userRoutes from "./routes/user.route.js"
 import adminRoutes from "./routes/admin.route.js"
 import authRoutes from "./routes/auth.route.js"
@@ -18,12 +18,19 @@ import statRoutes from "./routes/stat.route.js"
 
 dotenv.config();
 
+
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
-
 app.use(clerkMiddleware());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir:path.join(__dirname,"tmp"),
+}));
+
+
 
 
 app.use("/api/users",userRoutes)

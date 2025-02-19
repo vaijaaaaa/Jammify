90f54c82b41f38c6542db2fa678ddf18df71d4f7
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-19 23:30:09 +0530

    stat controller and route

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
 
 const router = Router();
 
-router.get("/",(req,res)=>{
-    res.send("stats router is being tested nice")
-})
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
